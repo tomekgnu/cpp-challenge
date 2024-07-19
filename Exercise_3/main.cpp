a6bc22ff16997bbbd4672568bc9444b5b4871459
Tomasz Urbaniak <tomekgnu@gmail.com>
2024-07-19 22:40:09 +0200

    - implemented LCM function for 2 arguments

--- Exercise_3/main.cpp
+++ Exercise_3/main.cpp
@@ -3,8 +3,26 @@
  *
  *  Created on: 19 lip 2024
  *      Author: Tomek
+ *
+ *      LEAST COMMON MULTIPLY
  */
 
+#include <numeric>
+#include <algorithm>
 
+constexpr unsigned int LCM(int a, int b){
+	unsigned long multiply = std::min(a,b);
+	while(true){
+		if(multiply % a == 0 && multiply % b == 0)
+			return multiply;
+		multiply++;
+	}
+}
 
+int main(void){
+	static_assert(LCM(1,2) == std::lcm(1,2));
+	static_assert(LCM(5,53) == std::lcm(5,53));
+
+	return 0;
+}
 
