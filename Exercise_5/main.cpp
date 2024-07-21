81323e485104ccd9ca84c608d25852570a1c63bc
Tomasz Urbaniak <tomekgnu@gmail.com>
2024-07-21 22:00:49 +0200

    - exercise 5: prime numbers different by 6

--- Exercise_5/main.cpp
+++ Exercise_5/main.cpp
@@ -1,11 +1,11 @@
 /*
  * main.cpp
  *
- *  Created on: 20 lip 2024
+ *  Created on: 21 lip 2024
  *      Author: Tomek
- *      Display numbers up to number typed by user
+ *      Display prime number different by 6 up to number given by user
+ *      E.g.: 1 7 13 19 23 29
  */
-
 
 #include <iostream>
 
@@ -22,23 +22,18 @@ bool isPrime(unsigned int n){
 
 int main(void){
 
-	unsigned int userNumber = 1;
-	//static_assert(isPrime(7) == true);
-	//static_assert(isPrime(4) == false);
-
+	unsigned int userNumber = 0;
 
 	std::cout << "Enter positive number: ";
 	std::cin >> userNumber;
 
-	while(userNumber > 1){
-		userNumber--;
-		if(isPrime(userNumber) == true){
-			break;
-		}
-
+	for(unsigned int i = 1; i < userNumber; i++){
+		if((i + 6) < userNumber && isPrime(i) && isPrime(i + 6))
+			std::cout << i << " " << i + 6 << "\n";
 	}
-
-	std::cout << userNumber << "\n";
 
 	return 0;
 }
+
+
+
